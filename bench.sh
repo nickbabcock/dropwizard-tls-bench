770297616351434da26a97f9966596d193123cdc
Nick Babcock <nbabcock19@hotmail.com>
2018-02-06 17:26:44 -0500

    Fix tabs in bench script

--- bench.sh
+++ bench.sh
@@ -28,21 +28,21 @@ for l in 0 10 100 1000 10000 100000; do
         BF="-Xbootclasspath/p:alpn-boot-8.1.11.v20170118.jar"
     fi;
 
-	if [[ $j == 'jersey' ]]; then
-		API_PATH='api'
-	else
-		API_PATH='perf'
-	fi;
+    if [[ $j == 'jersey' ]]; then
+        API_PATH='api'
+    else
+        API_PATH='perf'
+    fi;
 
     CMD="$BF -jar tls.bench-1.0-SNAPSHOT.jar  server config.yml"
     NAME=$(docker-compose run -d -e ENDPOINT_TYPE=$j -e APP_TYPE=$i -e JCE_PROVIDER=$k --service-ports web $CMD)
     sleep 2
 
-	"$NGHTTP2_DIR/src/h2load" -N 3s --duration=10 -c100 -m10 -d data-$l -t 2 "https://localhost:9443/$API_PATH" >/dev/null
-	
-	for m in {0..4}; do
-		REQ=$("$NGHTTP2_DIR/src/h2load" -N 3s -n100000 -c100 -m10 -d data-$l -t 2 "https://localhost:9443/$API_PATH" | grep finished | grep -o -P '(\d+\.\d+) (?=req/s)')
-		printf "$i,$j,$k,$l,$REQ\n" | tee -a out.csv
+    "$NGHTTP2_DIR/src/h2load" -N 3s --duration=10 -c100 -m10 -d data-$l -t 2 "https://localhost:9443/$API_PATH" >/dev/null
+
+    for m in {0..4}; do
+        REQ=$("$NGHTTP2_DIR/src/h2load" -N 3s -n100000 -c100 -m10 -d data-$l -t 2 "https://localhost:9443/$API_PATH" | grep finished | grep -o -P '(\d+\.\d+) (?=req/s)')
+        printf "$i,$j,$k,$l,$REQ\n" | tee -a out.csv
     done;
 
     docker stop $NAME
